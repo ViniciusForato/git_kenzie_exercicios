920c6e181c70a158c938b8cc139c56e035edd453
ViniciusForato <110116361+ViniciusForato@users.noreply.github.com>
2022-08-11 14:50:43 -0300

    Add files via upload

---
+++ M1/exercicio S3-08/script.js
@@ -0,0 +1,8 @@
+let nome = 'Vinicius';
+console.log(`eu sou o ${nome}`);
+
+let sobrenome = 'Forato';
+console.log(`Olá me chamo ${nome} ${sobrenome}`);
+
+let idade = 20;
+console.log(`Olá me chamo ${nome} ${sobrenome} e tenho ${idade} anos`);
---
+++ M1/m1-s3-entrega-praticando-com-variaveis-ViniciusForato/script.js
@@ -0,0 +1,7 @@
+let nome = 'Vinicius';
+let sobrenome = 'Forato';
+let anoNascimento = 2001;
+let anoAtual = 2022;
+let idade = anoAtual - anoNascimento;
+let cidade = 'Caraguatatuba';
+console.log(`Eu sou ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
