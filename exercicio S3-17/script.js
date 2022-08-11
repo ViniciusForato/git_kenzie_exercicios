a86c7444ef5f60a79c35d3a9a67224999c6824ff
ViniciusForato <110116361+ViniciusForato@users.noreply.github.com>
2022-08-11 14:45:07 -0300

    Add files via upload

---
+++ exercicio S3-17/script.js
@@ -0,0 +1,14 @@
+let usuarioAutenticado = true;
+    if (usuarioAutenticado = true){
+        console.log('seja bem-vindo')
+    }else(
+        console.log('usuário inválido')
+    );
+
+let usuario = 'Ana';
+let autenticado = false;
+ if(autenticado = true){
+    console.log('olá Ana, voçê está autenticada')
+ }else(
+    console.log('usuário inválido')
+ )
